fix: intrisic reward computation and thresholds

autodr_IR.py
@@ -109,7 +109,7 @@ phi_L = np.array([3.5, 2.3, 4.6, 0.001, 0.001, 0.001])
 phi_H = np.array([4.5, 3.3, 5.5, 0.5, 0.5, 0.5])
 phi_min = np.array([0.1, 0.1, 0.1, 0, 0, 0])
 delta = 0.1 
-t_L, t_H = 0.2, 1.5  # Adjust these thresholds as needed
+t_L, t_H = 0.2, 1.0   # Adjust these thresholds as needed
 m = 10
 
 D_L = [[] for _ in range(6)]
@@ -120,9 +120,10 @@ stability_history = []
 balance_history = []
 entropy_ADR_history = []
 intrinsic_reward_history = []
+intrinsic_reward = 0
 
 # Open file to save reward and parameters
-with open('reward_and_parameters_RND.txt', 'w') as f:
+with open('reward_and_parameters_RND1.txt', 'w') as f:
     for episode in range(10000):
         lambda_vec = np.random.uniform(phi_L, phi_H)
         i = np.random.choice(6)
@@ -142,8 +143,8 @@ with open('reward_and_parameters_RND.txt', 'w') as f:
             new_obs, extrinsic_reward, done, info = wrapped_env.step(action)
             
             # Compute intrinsic reward using RND
-            intrinsic_reward = compute_intrinsic_reward(new_obs, target_network, predictor_network)
-            intrinsic_reward_history.append(intrinsic_reward)
+            timestep_intrinsic_reward = compute_intrinsic_reward(new_obs, target_network, predictor_network)
+            intrinsic_reward += timestep_intrinsic_reward
             # Combine intrinsic and extrinsic rewards
             total_reward = extrinsic_reward + intrinsic_reward
             # Update predictor network with the new state
@@ -158,7 +159,8 @@ with open('reward_and_parameters_RND.txt', 'w') as f:
         ## Entropy calculation with a small epsilon to avoid NaN
         epsilon = 1e-8
         entropy = np.mean(np.log(np.maximum(phi_H - phi_L, epsilon)))
-                          
+
+        intrinsic_reward_history.append(intrinsic_reward)                  
         performance_history.append(extrinsic_reward)
         speed_history.append(speed)
         stability_history.append(stability)
@@ -210,36 +212,36 @@ episodes = np.arange(len(performance_history))
 plt.figure()
 plot_with_shading(episodes, performance_history, 'darkblue', 'lightblue', 'Total Reward', 'Reward', 'Performance of the Model')
 plt.tight_layout()
-plt.savefig('performance_IR.png')
+plt.savefig('performance_IR1.png')
 
 # Plot Speed
 plt.figure()
 plot_with_shading(episodes, speed_history, 'darkgreen', 'lightgreen', 'Speed', 'Speed', 'Speed Throughout Training')
 plt.tight_layout()
-plt.savefig('speed_IR.png')
+plt.savefig('speed_IR1.png')
 
 # Plot Stability
 plt.figure()
 plot_with_shading(episodes, stability_history, 'darkorange', 'navajowhite', 'Stability', 'Stability', 'Stability Throughout Training')
 plt.tight_layout()
-plt.savefig('stability_IR.png')
+plt.savefig('stability_IR1.png')
 
 # Plot Balance
 plt.figure()
 plot_with_shading(episodes, balance_history, 'darkred', 'lightcoral', 'Balance', 'Balance', 'Balance Throughout Training')
 plt.tight_layout()
-plt.savefig('balance_IR.png')
+plt.savefig('balance_IR1.png')
 
 # Plot ADR entropy
 plt.figure()
 plot_with_shading(episodes, entropy_ADR_history, 'purple', 'plum', 'ADR Entropy', 'Entropy', 'ADR Entropy Throughout Training')
 plt.tight_layout()
-plt.savefig('entropy_IR.png')
+plt.savefig('entropy_IR1.png')
 
 # Plot ADR entropy
 plt.figure()
 plot_with_shading(episodes, intrinsic_reward_history, 'teal', 'lightcyan', 'Intrinsic Reward', 'Intrinsic Reward', 'ADR Entropy Throughout Training')
 plt.tight_layout()
-plt.savefig('intrinsic_reward_IR.png')
+plt.savefig('intrinsic_reward_IR1.png')
 
 plt.show()